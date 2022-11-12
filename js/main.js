00cd7d9b9255d3d160f671f6ddd8276ba2cf5011
eldar mutaev <eldarmutaev@inbox.ru>
2022-11-12 23:09:42 +0300

    desk fihish

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+
+  // And if we need scrollbar
+
+});
